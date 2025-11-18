648173e6e68152180afd49be7bd4211431fd5031
Jorgeluis006 <cuentaparajugarenlan007@gmail.com>
2025-11-17 19:57:55 -0500

    Aumentar tamaño de logos y remover fondo negro

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         {/* Logo centrado arriba */}
         <div className="text-center mb-10">
           <div className="flex items-center justify-center mb-3">
-            <img src="/images/B2.png" alt="Atisbe Academy Logo" className="h-20 w-auto" />
+            <img src="/images/logofooter.jpeg" alt="Atisbe Academy Logo" className="h-32 w-auto" style={{ backgroundColor: 'transparent' }} />
           </div>
         </div>
 
